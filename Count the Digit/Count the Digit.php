c8e9406693e39733fe47ad8e19c204f674518f35
poshimy <yura.pisyn@yandex.ru>
2021-11-27 17:04:42 +0300

    1

--- Count the Digit/Count the Digit.php
+++ Count the Digit/Count the Digit.php
@@ -3,10 +3,4 @@
     $k='';
   for($i=0;$i<=$n;$i++){
     $k .= (string)$i**2;
-<<<<<<< HEAD
-=======
-  }
-  $str = substr_count($k,(string)$d);
-  return $str;
->>>>>>> 46d06badb0321d88f28edcf76a3371d1b205bee7
 }
