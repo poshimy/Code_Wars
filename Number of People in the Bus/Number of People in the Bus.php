d015a708663043cdb565cce62583779e9320b250
poshimy <yura.pisyn@yandex.ru>
2021-11-27 16:29:12 +0300

    Number of People in the Bus

--- Number of People in the Bus/Number of People in the Bus.php
+++ Number of People in the Bus/Number of People in the Bus.php
@@ -1,8 +1,7 @@
 <?php
- $c=0;
- for($i=0;$i<count($bus_stops);$i++){
-   $c += intval($bus_stops[$i][0]);
-   $c -= intval($bus_stops[$i][1]);
-   }
- return $c;
- 
+$c=0;
+for($i=0;$i<count($bus_stops);$i++){
+  $c += intval($bus_stops[$i][0]);
+  $c -= intval($bus_stops[$i][1]);
+}
+return $c;
